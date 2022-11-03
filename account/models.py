d4ab967797efe688d6434def4ee77f9c25a47aec
Shahab Badihi <badihi2000@gmail.com>
2022-11-03 21:10:30 +0330

    form reqired

--- account/models.py
+++ account/models.py
@@ -22,25 +22,25 @@ class Report(models.Model):
         (2, 'select 2'),
         (3, 'select 3'),
     )
-    client_name = models.CharField(max_length=300, null=True, blank=True, verbose_name='Client Name')
-    report_date = models.DateField(null=True, blank=True, verbose_name='Report Date')
-    effective_date = models.DateField(null=True, blank=True, verbose_name='Effective Date')
-    ref_code = models.CharField(max_length=300, null=True, blank=True, verbose_name='Ref Code')
-    firstname = models.CharField(max_length=300, null=True, blank=True, verbose_name='First Name')
-    lastname = models.CharField(max_length=300, null=True, blank=True, verbose_name='Last Name')
-    email = models.CharField(max_length=300, null=True, blank=True, verbose_name='Email Address')
-    phone_number = models.CharField(max_length=300, null=True, blank=True, verbose_name='Phone Number')
-    location = models.TextField(max_length=300, null=True, blank=True, verbose_name='Location')
-    latitude = models.FloatField(null=True, blank=True, verbose_name='Latitude')
-    longitude = models.FloatField(null=True, blank=True, verbose_name='Longitude')
-    dropdown_feature = models.IntegerField(choices=DROPDOWN_CHOICES, blank=True, null=True,
+    client_name = models.CharField(max_length=300, null=True, blank=False, verbose_name='Client Name')
+    report_date = models.DateField(null=True, blank=False, verbose_name='Report Date')
+    effective_date = models.DateField(null=True, blank=False, verbose_name='Effective Date')
+    ref_code = models.CharField(max_length=300, null=True, blank=False, verbose_name='Ref Code')
+    firstname = models.CharField(max_length=300, null=True, blank=False, verbose_name='First Name')
+    lastname = models.CharField(max_length=300, null=True, blank=False, verbose_name='Last Name')
+    email = models.CharField(max_length=300, null=True, blank=False, verbose_name='Email Address')
+    phone_number = models.CharField(max_length=300, null=True, blank=False, verbose_name='Phone Number')
+    location = models.TextField(max_length=300, null=True, blank=False, verbose_name='Location')
+    latitude = models.FloatField(null=True, blank=False, verbose_name='Latitude')
+    longitude = models.FloatField(null=True, blank=False, verbose_name='Longitude')
+    dropdown_feature = models.IntegerField(choices=DROPDOWN_CHOICES, blank=False, null=True,
                                            verbose_name='Dropdown Feature')
-    textarea1 = models.TextField(max_length=300, null=True, blank=True, verbose_name='TextArea 1')
-    textarea2 = models.TextField(max_length=300, null=True, blank=True, verbose_name='TextArea 2')
-    textarea3 = models.TextField(max_length=300, null=True, blank=True, verbose_name='TextArea 3')
-    radio_button_feature = models.IntegerField(choices=RADIO_BUTTON_CHOICES, null=True, blank=True,
+    textarea1 = models.TextField(max_length=300, null=True, blank=False, verbose_name='TextArea 1')
+    textarea2 = models.TextField(max_length=300, null=True, blank=False, verbose_name='TextArea 2')
+    textarea3 = models.TextField(max_length=300, null=True, blank=False, verbose_name='TextArea 3')
+    radio_button_feature = models.IntegerField(choices=RADIO_BUTTON_CHOICES, null=True, blank=False,
                                                verbose_name='Radio Button Feature')
-    receive_newsletter = models.BooleanField(null=True, blank=True, verbose_name='Receive NewsLetter')
-    fpdf = models.FileField(upload_to='reports/%Y/%m/%d/',null=True, blank=True, verbose_name='Report File')
+    receive_newsletter = models.BooleanField(null=True, blank=False, verbose_name='Receive NewsLetter')
+    fpdf = models.FileField(upload_to='reports/%Y/%m/%d/',null=True, blank=False, verbose_name='Report File')
 
     
