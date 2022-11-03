2fc1e210c4d7f36560e427e26baa48f903cf9fe3
Shahab Badihi <badihi2000@gmail.com>
2022-11-03 14:59:26 +0330

    home url as account

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('account.urls')),
-    path('pdf/', include('pdf.urls'))
+    path('pdf/', include('pdf.urls')),
+    path('', include('account.urls')),
 ]
